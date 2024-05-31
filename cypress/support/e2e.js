518d6a0bf709017b4c191f551e34482af971be2e
Chibuzor Obilom <chibuzor.obilom@gmail.com>
2024-05-31 16:49:39 +0100

    Pushing Working Cypress Tests

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,5 +16,30 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+// ***********************************************************
+
+/*   before('Testing Before All', () => {
+    cy.log('Start BEFORE ALL tests')
+  }) */
+
+beforeEach('Testing Before Each', () => {
+    // cy.log('Start BEFORE EACH test')
+    Cypress.on('uncaught:exception', () => {
+        return false;
+    })
+    cy.visit('/')
+    cy.get('.Header2_header__nav__btn__wRKfY div:nth-child(2) button').should('be.visible').click()
+})
+
+/*   after('Testing After All', () => {
+    cy.log('Start AFTER ALL tests end')
+  }) */
+
+/*   afterEach('Testing After Each', () => {
+    cy.log('Start AFTER EACH test ends')
+  }) */
+
+// ***********************************************************
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
